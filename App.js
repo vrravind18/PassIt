c5cd7a8a4e25e80bb17dd062acd10d892c10f6d7
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-05-07 15:33:14 -0400

    add launch.json for debuggin on chrome, added images to app.js

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+  console
+  const handlePress = () => console.log("Text Pressed.")
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text number numberOfLines={1} onPress= {handlePress}>
+        Hello world
+      </Text>
+      <Image source = {{
+        uri: "https://picsum.photos/200/300",
+        width: 200,
+        height: 300}}/>
+        {/* <Image source = {require("./assets/icon.png")}/> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center"
   },
 });
