ce8642205d46c7d36576b1ca1d0be2f017f34f40
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-05-10 11:13:23 -0400

    Added Welcome screen

--- App.js
+++ App.js
@@ -1,42 +1,18 @@
-import React from 'react';
-import { 
-  StyleSheet, 
-  Text, 
+import React from "react";
+import {
+  StyleSheet,
+  Text,
   View,
   Button,
-  TouchableHighlight, 
-  Image, 
+  TouchableHighlight,
+  Image,
   Alert,
-  SafeAreaView 
-} from 'react-native';
+  SafeAreaView,
+} from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  console
-  const handlePress = () => console.log("Text Pressed.")
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text number numberOfLines={1} onPress= {handlePress}>
-        Hello React Native world
-        <TouchableHighlight onPress={() => console.log("Image tapped")}>
-            <Image source = {{
-            uri: "https://picsum.photos/200/300",
-            width: 200,
-            height: 300}}/>
-            {/* <Image source = {require("./assets/icon.png")}/> */}
-        </TouchableHighlight>
-      </Text>
-      <Button 
-        title="Click me"
-        onPress={() => 
-          Alert.alert("My title", "My msg", [
-          { text: "Yes", onPress: () => console.log("Yes")}, 
-          {text:"No", onPress: () => console.log("No")}
-          ])
-        }
-      />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
@@ -44,6 +20,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
 });
