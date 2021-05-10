030f89c359bebf07b21bf743a84f59257d607f3d
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-05-10 11:37:54 -0400

    added 2 screens and added config folder for colors

--- App.js
+++ App.js
@@ -9,16 +9,18 @@ import {
   Alert,
   SafeAreaView,
 } from "react-native";
+import colors from "./app/config/colors";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.white,
     justifyContent: "center",
     alignItems: "center",
   },
