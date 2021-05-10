c92860161495268f08628f07d817af4fd3ff3130
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-05-10 08:34:45 -0400

    added Buttons

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  Button,
+  TouchableHighlight, 
+  Image, 
+  Alert,
+  SafeAreaView 
+} from 'react-native';
 
 export default function App() {
   console
@@ -9,13 +17,24 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text number numberOfLines={1} onPress= {handlePress}>
-        Hello world
+        Hello React Native world
+        <TouchableHighlight onPress={() => console.log("Image tapped")}>
+            <Image source = {{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 300}}/>
+            {/* <Image source = {require("./assets/icon.png")}/> */}
+        </TouchableHighlight>
       </Text>
-      <Image source = {{
-        uri: "https://picsum.photos/200/300",
-        width: 200,
-        height: 300}}/>
-        {/* <Image source = {require("./assets/icon.png")}/> */}
+      <Button 
+        title="Click me"
+        onPress={() => 
+          Alert.alert("My title", "My msg", [
+          { text: "Yes", onPress: () => console.log("Yes")}, 
+          {text:"No", onPress: () => console.log("No")}
+          ])
+        }
+      />
     </SafeAreaView>
   );
 }
