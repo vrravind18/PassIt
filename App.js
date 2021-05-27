0f4fc4884e31cedf6695dbe814c8397ee6a1913f
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-05-27 11:45:27 -0400

    Updated App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
+import FeedNavigator from "./app/navigation/FeedNavigator";
 
 export default function App() {
   return (
